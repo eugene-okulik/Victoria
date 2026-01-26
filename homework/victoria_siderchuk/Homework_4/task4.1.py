4d28c7bc4e5dc21191a6d0eca96a62706d807158
Victoria Siderchuk <siderchuk@gmail.com>
2026-01-26 18:39:16 +0300

    Handle lint errors

--- homework/victoria_siderchuk/Homework_4/task4.1.py
+++ homework/victoria_siderchuk/Homework_4/task4.1.py
@@ -7,16 +7,11 @@ print(my_dict['tuple'][-1])
 
 my_dict['list'].append(120)
 my_dict['list'].pop(1)
-#print(my_dict['list'])
 
 my_dict['dict'][('i am a tuple',)] = ('green', 'blue')
-#print(my_dict['dict'])
 my_dict['dict'].pop('key4')
-#print(my_dict['dict'])
 
 my_dict['set'].add('True')
-#print(my_dict['set'])
 my_dict['set'].pop()
-#print(my_dict['set'])
 
 print(my_dict)
