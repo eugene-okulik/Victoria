42eba4c00fd2b5a7d54f5099938876475b513b36
Victoria Siderchuk <siderchuk@gmail.com>
2026-02-07 00:29:21 +0300

    Lint errors fixed

--- homework/victoria_siderchuk/Homework_7/task7.3.py
+++ homework/victoria_siderchuk/Homework_7/task7.3.py
@@ -5,20 +5,24 @@ text4 = 'результат: 2'
 
 numb_to_add = 10
 
+
 # Using index
 def calc(result):
     sum = int(result[result.index(':') + 2:]) + numb_to_add
     print(sum)
+
 
 calc(text1)
 calc(text2)
 calc(text3)
 calc(text4)
 
+
 # Using split
 def calc2(result2):
     sum2 = int(result2.split()[-1]) + numb_to_add
     print(sum2)
+
 
 calc2(text1)
 calc2(text2)
