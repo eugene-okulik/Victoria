92214d5852ef51da75cb8f2bc9d76684f0f56702
Victoria Siderchuk <siderchuk@gmail.com>
2026-02-10 01:17:39 +0300

    Lint errors fixed

--- homework/victoria_siderchuk/Homework_8/task8.1.py
+++ homework/victoria_siderchuk/Homework_8/task8.1.py
@@ -2,12 +2,11 @@ import random
 
 salary = int(input('What is your salary?'))
 bonus = random.choice([True, False])
-random_bonus = int(random.random()*100)
+random_bonus = int(random.random() * 100)
 
-if bonus == True:
+if bonus is True:
     final_salary = salary + random_bonus
 else:
     final_salary = salary
 
 print(f"{salary}, {bonus} - '${final_salary}'")
-
